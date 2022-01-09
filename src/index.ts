464e955dabe44a694458f7f9bc09117cb09a8654
xiaodi.yan <yan_xiaodi@hotmail.com>
2022-01-10 11:47:39 +1300

    Optional interface properties

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,7 @@ import { User } from './models/User';
 const user = new User({ name: 'Xiaodi', age: 20 });
 console.log(user.get('name'));
 console.log(user.get('age'));
+
+user.set({ name: 'Tom' });
+console.log(user.get('name'));
+console.log(user.get('age'));
