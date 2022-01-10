963fbbdbf210ece72f3257b59cee359196601092
xiaodi.yan <yan_xiaodi@hotmail.com>
2022-01-11 10:54:27 +1300

    Add events system

--- src/models/User.ts
+++ src/models/User.ts
@@ -2,7 +2,11 @@ interface UserProps {
   name?: string;
   age?: number;
 }
+
+type Callback = () => void;
+
 export class User {
+  events: { [key: string]: Callback[] } = {};
   constructor(private data: UserProps) {}
 
   get(propName: string): number | string {
@@ -11,5 +15,21 @@ export class User {
 
   set(update: UserProps) {
     Object.assign(this.data, update);
+  }
+
+  on(eventName: string, callback: Callback): void {
+    const handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  }
+
+  trigger(eventName: string) {
+    const handlers = this.events[eventName];
+    if (!handlers || handlers.length === 0) {
+      return;
+    }
+    handlers.forEach(callback => {
+      callback();
+    });
   }
 }
