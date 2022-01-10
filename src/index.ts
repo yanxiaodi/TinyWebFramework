4cb28e1761238cc0f29dd0bf2f0d76d39094e0c1
xiaodi.yan <yan_xiaodi@hotmail.com>
2022-01-11 10:54:59 +1300

    Add json-server

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,28 @@
-import { User } from './models/User';
+// import { User } from './models/User';
 
-const user = new User({ name: 'Xiaodi', age: 20 });
-console.log(user.get('name'));
-console.log(user.get('age'));
+// const user = new User({ name: 'Xiaodi', age: 20 });
+// console.log(user.get('name'));
+// console.log(user.get('age'));
 
-user.set({ name: 'Tom' });
-console.log(user.get('name'));
-console.log(user.get('age'));
+// user.set({ name: 'Tom' });
+// console.log(user.get('name'));
+// console.log(user.get('age'));
+
+// user.on('change', () => {
+//   console.log('test #1');
+// });
+// user.on('change', () => {
+//   console.log('test #2');
+// });
+// user.on('save', () => {
+//   console.log('save triggered');
+// });
+// console.log(user);
+// user.trigger('change');
+
+import axios from 'axios';
+
+axios.post('http://localhost:3000/users', {
+  name: 'myname',
+  age: 20,
+});
