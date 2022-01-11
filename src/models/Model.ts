b0e036daa15ada1478a82d59f509b61d042b6ad5
xiaodi.yan <yan_xiaodi@hotmail.com>
2022-01-11 21:28:10 +1300

    Refactor Model base class

---
+++ src/models/Model.ts
@@ -0,0 +1,62 @@
+import { AxiosPromise, AxiosResponse } from 'axios';
+import { HasId } from './ApiSync';
+
+interface ModelAttributes<T> {
+  set(value: T): void;
+  getAll(): T;
+  get<K extends keyof T>(key: K): T[K];
+}
+interface Sync<T> {
+  fetch(id: number): AxiosPromise;
+  save(data: T): AxiosPromise;
+}
+interface Events {
+  on(eventName: string, callback: () => void): void;
+  trigger(eventName: string): void;
+}
+
+export class Model<T extends HasId> {
+  constructor(
+    private attributes: ModelAttributes<T>,
+    private events: Events,
+    private sync: Sync<T>
+  ) {}
+
+  get on() {
+    return this.events.on;
+  }
+
+  get trigger() {
+    return this.events.trigger;
+  }
+
+  get get() {
+    return this.attributes.get;
+  }
+
+  set(update: T) {
+    this.attributes.set(update);
+    this.events.trigger('change');
+  }
+
+  fetch() {
+    const id = this.attributes.get('id');
+    if (typeof id !== 'number') {
+      throw new Error('Cannot fetch without an id');
+    }
+    this.sync.fetch(id).then((response: AxiosResponse) => {
+      this.set(response.data);
+    });
+  }
+
+  save() {
+    this.sync
+      .save(this.attributes.getAll())
+      .then((response: AxiosResponse) => {
+        this.trigger('save');
+      })
+      .catch(() => {
+        this.trigger('error');
+      });
+  }
+}
